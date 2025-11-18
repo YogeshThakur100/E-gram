992cad74fc82af6243b21e1f09d415e154495202
Mohanadithya4455 <mohanrankireddy4455@gmail.com>
2025-11-18 11:46:59 +0530

    Fixed namuna9 get all recoreds api

--- namuna9/namuna9_apis.py
+++ namuna9/namuna9_apis.py
@@ -704,7 +704,8 @@ def get_namuna9_table_data_custom(
                     'chaluCleaningTax': t_clean
                 }
 
-    properties = db.query(namuna8_model.Property).filter(namuna8_model.Property.id.in_([int(i) for i in property_ids])).all()
+    properties = db.query(namuna8_model.Property).filter(namuna8_model.Property.id.in_([int(i) for i in property_ids]),
+                                                         namuna8_model.Property.village_id == villageId).all()
     rows = []
     for idx, prop in enumerate(properties, 1):
         prop_data = build_property_response(prop, db, gram_panchayat_id)
